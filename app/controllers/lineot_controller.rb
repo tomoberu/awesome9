3a850678e7b6b69ceb4c2d8fbb649f93d05c09a6
tomoberu <57937124+tomoberu@users.noreply.github.com>
2020-01-27 18:57:02 +0900

    コミット

--- app/controllers/lineot_controller.rb
+++ app/controllers/lineot_controller.rb
@@ -4,7 +4,6 @@ class LineotController < ApplicationController
 
   def callback
     body = request.body.read
-
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
